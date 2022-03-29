0a4fa08b056c0cbd1d41dfb3531ca8ab15dc7220
chaos <1065807387@qq.com>
2022-03-29 18:01:24 +0800

    comp

---
+++ SSM/ssm_crud.sql
@@ -0,0 +1,25 @@
+
+CREATE DATABASE ssm_crud
+
+CREATE TABLE IF NOT EXISTS `ssm_crud`.`tbl_emp`(  
+  `emp_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `emp_name` VARCHAR(255) NOT NULL,
+  `gender` CHAR(1),
+  `email` VARCHAR(255),
+  `d_id` INT(11),
+  PRIMARY KEY (`emp_id`)
+);
+SELECT * FROM tbl_emp;
+
+
+CREATE TABLE IF NOT EXISTS `ssm_crud`.`tbl_dept`(  
+  `dept_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `dept_name` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`dept_id`)
+);
+SELECT * FROM tbl_dept;
+
+ALTER TABLE `ssm_crud`.`tbl_emp`  
+  ADD CONSTRAINT `fk_emp_dept` FOREIGN KEY (`d_id`) REFERENCES `ssm_crud`.`tbl_dept`(`dept_id`);
+
+#drop database `ssm-crud`;
