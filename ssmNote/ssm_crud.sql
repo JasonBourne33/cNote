0a7ebdbb7c66a5bbb4261473fc06593c8ba1e19e
chaos <1065807387@qq.com>
2022-04-06 08:04:34 +0800

    home

--- ssmNote/ssm_crud.sql
+++ ssmNote/ssm_crud.sql
@@ -17,6 +17,7 @@ CREATE TABLE IF NOT EXISTS `ssm_crud`.`tbl_dept`(
   PRIMARY KEY (`dept_id`)
 );
 SELECT * FROM tbl_dept;
+DELETE * FROM tbl_dept;
 
 ALTER TABLE `ssm_crud`.`tbl_emp`  
   ADD CONSTRAINT `fk_emp_dept` FOREIGN KEY (`d_id`) REFERENCES `ssm_crud`.`tbl_dept`(`dept_id`);
